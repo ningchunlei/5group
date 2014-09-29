04365ecbb7f2904ecbe5a597a6aa48ad24fa8b85
jack <ningchunlei@gmail.com>
2014-09-29 23:31:18 +0800

    init

--- mmgroup/settings.py
+++ mmgroup/settings.py
@@ -46,8 +46,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 )
 
 ROOT_URLCONF = 'mmgroup.urls'
